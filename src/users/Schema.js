839acafd1d70e5df533e77ccccf9e494c165586e
unknown <lordshegz@gmail.com>
2021-01-10 14:19:50 +0000

    fixed phone validation and changeed age to dob

--- src/users/Schema.js
+++ src/users/Schema.js
@@ -7,62 +7,75 @@ const bcrypt = require("bcrypt");
 
 
 const userSchema = new Schema({
-    name: {
-      type: String,
-      required: true
-    },
-    surname: {
-      type: String,
-      required: true,
-    },
-    username: {
-      type: String,
-      lowercase: true,
-      required: true,
-
-    },
-    phone:{
-      type: Number,
-      required: true,
-      minlength: 11,
-      maxlength:15
-    },
-    age:{
-      type: Number,
-      required: true,
-      min: 18
-    },
-    password: {
-      type: String,
-      required: true,
-      minlength: 7
-    },
-    role: {
-      type: String,
-      required: true,
-      trim: true,
-      default: 'user'
-    },
-    email: {
-      type: String,
-      lowercase: true,
-      trim: true,
-      validate: [
-        validator({
-          validator: 'isEmail',
-          message: 'Oops..please enter valid email'
-        })
-      ],
-      required: true
-    },
-    refreshTokens: [{
-      token: {
-        type: String,
-        required: true,
-      },
-    }, ],
+  name: {
+    type: String,
+    required: true
+  },
+  surname: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    lowercase: true,
+    required: true,
 
   },
+  phone: {
+    type: String,
+    validate: {
+      validator: function(v) {
+        return /0\d{2}\d{4}\d{4}/.test(v);
+      },
+      message: 'Phone number is not valid, allows only 11 digits'
+    },
+    required: [true, 'User phone number required']
+  },
+
+  dob: {
+    type: Date,
+    validate: {
+      validator: function (v) {
+        v.setFullYear(v.getFullYear() + 18)
+        const currentTime = new Date();
+        currentTime.setHours(0, 0, 0, 0);
+        return v.getTime() <= currentTime.getTime();
+      },
+      message: props => 'You must be 18 years old.'
+    },
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 7
+  },
+  role: {
+    type: String,
+    required: true,
+    trim: true,
+    default: 'user'
+  },
+  email: {
+    type: String,
+    lowercase: true,
+    trim: true,
+    validate: [
+      validator({
+        validator: 'isEmail',
+        message: 'Oops..please enter valid email'
+      })
+    ],
+    required: true
+  },
+  refreshTokens: [{
+    token: {
+      type: String,
+      required: true,
+    },
+  },],
+
+},
 
   {
     timestamps: true
