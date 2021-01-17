b9c7a3c30aba78f307a561fcc32bcd21bda732e3
Emmanuel Adedeji <emmans4destiny@gmail.com>
2021-01-17 16:02:41 +0200

    Email activation added

--- src/users/index.js
+++ src/users/index.js
@@ -1,36 +1,110 @@
-const express = require("express")
-const UserModel = require("./Schema")
+const express = require("express");
+const UserModel = require("./Schema");
+const mailgun = require("mailgun-js");
+const jwt = require("jsonwebtoken")
+const DOMAIN = process.env.DOMAIN;
+const mg = mailgun({apiKey: process.env.MAILGUN_APIKEY, domain: DOMAIN});
 const {
   authenticate,
   refreshToken
-} = require("../auth/authTools")
+} = require("../auth/authTools");
 const {
   authorize,
   adminOnlyMiddleware
-} = require("../middlewares/authorize")
-const router = express.Router()
-const bcrypt = require("bcrypt");
-const passport = require("passport");
+} = require("../middlewares/authorize");
+const { json } = require("express");
+const router = express.Router();
+// const bcrypt = require("bcrypt");
+// const passport = require("passport");
 
 router.post("/register", async (req, res) => {
   try {
-    const checkEmail = await UserModel.find({
-      email: req.body.email
-    });
-    console.log(checkEmail);
-    if (checkEmail.length !== 0) {
-      res.status(409).send("user with same email exists");
-    } else {
-
-      const newUser = new UserModel(req.body);
-      await newUser.save();
-      res.status(201).send("registered successfuly");
-    }
+    const {name,surname,username,phone,dob,email,password,role}=req.body;
+  
+    UserModel.findOne({email}).exec((err, user)=>{
+      if(user){
+        return res.status(409).send("user with same email exists");
+        
+      }
+      
+      const token = jwt.sign(
+        {name,surname,username,phone,dob,email,password,role},
+        process.env.ACC_ACTIVATION_KEY, {
+          expiresIn: "30m"
+        }
+      );
+      
+      const data = {
+        from: 'noreply@betsoka.com.ng',
+        to: email,
+        subject: 'Account Activation Link',
+        html: `<h2> Please click on given link to activate your account</h2>
+        <a href="${process.env.CLIENT_URL}/authentication/activate/${token}">Activate your account!</a>
+        <p>${process.env.CLIENT_URL}/authentication/activate/${token}</>
+        `
+      };
+      mg.messages().send(data, function (error, body) {
+        if(error){
+          return res.json({
+            error: err.message
+          })
+        }
+        return res.json({message: "Email has been sent kindly activate your account"})
+      });
+     
+    })
+    
+    
   } catch (error) {
-    //next(error);
+    
     res.send(error.errors);
   }
 });
+router.post("/email-activate", async (req, res, next) => {
+  try {
+    const {
+      token
+    } = req.body;
+    if(token){
+      jwt.verify(token, process.env.ACC_ACTIVATION_KEY, function (err, decodedToken) {
+        if(err){
+          return res.status(400).json({error: "Incorrect or Expired link."})
+        }
+       
+        try {
+          const {name,surname,username,phone,dob,email,password,role}=decodedToken;
+          console.log(decodedToken)
+          UserModel.findOne({email}).exec((err, user)=>{
+            if(user){
+              return res.status(409).send("user with same email exists");
+              
+            }
+            let newUser = new UserModel({name,surname,username,phone,dob,email,password,role});
+            newUser.save((err, success)=>{
+              if(err){
+                console.log("Error in signup", err)
+                return res.status(400), json({error: err})
+              }
+              res.json({message: "Register successfully!"})
+            });
+          })
+        
+        } catch (error) {
+          
+          res.send(error.errors);
+        }
+        
+      })
+    }else{
+      return res.json({error: "Invalid activation please check your email"});
+    }
+    
+  } catch (error) {
+    next(error)
+  }
+
+})
+
 router.get("/", adminOnlyMiddleware, async (req, res, next) => {
   try {
     const users = await UserModel.find(req.query)
