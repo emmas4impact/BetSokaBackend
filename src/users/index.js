f600d03c497e021408febcb12c6231d45d76904d
Emmanuel Adedeji <emmans4destiny@gmail.com>
2021-01-26 00:03:03 +0200

    Update index.js

--- src/users/index.js
+++ src/users/index.js
@@ -294,8 +294,8 @@ router.post("/login", async (req, res, next) => {
     //     path: "/users/refreshToken",
   // })
     if(user){
-      res.send(req.user);
-      //res.send("login successfully")
+      res.send(user);
+      res.send("login successfully")
       
     }
     
