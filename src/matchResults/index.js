bfb8c1f5fd958d423a363242a7406d6cc4bc0389
Emmanuel Adedeji <emmans4destiny@gmail.com>
2021-02-02 23:32:23 +0200

    post routes done

--- src/matchResults/index.js
+++ src/matchResults/index.js
@@ -1,14 +1,8 @@
 const express =require('express');
 const ResultModel = require("./Schema");
-
-  const {
-    authorize,
-    adminOnlyMiddleware
-  } = require("../middlewares/authorize");
-  const { json } = require("express");
-  const result = express.Router();
+const result = express.Router();
   
-  result.get("/", authorize, adminOnlyMiddleware, async (req, res, next)=>{
+  result.get("/",  async (req, res, next)=>{
      try {
         const showResult = await ResultModel.find(req.params);
         if(showResult){
@@ -22,27 +16,33 @@ const ResultModel = require("./Schema");
          next(error)
      }
   })
-  result.post("/",authorize, adminOnlyMiddleware, async(req, res, next)=>{
+  result.post("/",async(req, res, next)=>{
       try {
+        const newMatchResult = new ResultModel({
+            ...req.body
+            
+        });
+        const savedMatchResult = await newMatchResult.save();
+        if(savedMatchResult ){
+            res.status(201).send(savedMatchResult );
+        }else{
+            res.status(404).json({message: "Please check match result"});
+        }
           
       } catch (error) {
           next(error);
       }
       
   })
-  result.delete("/:id", authorize, adminOnlyMiddleware, async (req, res, next)=>{
+  result.delete("/:id", async (req, res, next)=>{
       try {
           const deleteMatchResult = await ResultModel.findByIdAndDelete(req.params.id);
           if(deleteMatchResult){
             res.status(202).json({message: "Record deleted successfully"});
               
           }else{
-              
-          }
-
-        
-          
-          
+            res.status(404).json({message: "Record id invalid"});     
+          }    
       } catch (error) {
           next(error);
       }
