65d9a3a2cef4aafa0f71d76cbe600fd520dd986d
Emmanuel Adedeji <emmans4destiny@gmail.com>
2021-02-09 20:16:09 +0200

    Update Schema.js

--- src/accounts/Schema.js
+++ src/accounts/Schema.js
@@ -1,65 +1,70 @@
-const { model, Schema } = require("mongoose");
+// const { model, Schema } = require("mongoose");
 
-const AccountSchema = new Schema(
-  {
-    accountName: {
-      type: String,
-      required: true,
-    },
-    Bank: {
-      type: String,
-      required: true,
-    },
+// const AccountSchema = new Schema(
+//   {
+//     accountName: {
+//       type: String,
+//       required: true,
+//     },
+//     Bank: {
+//       type: String,
+//       required: true,
+//     },
 
-    accountNumber: {
-      type: Number,
-      required: true,
-    },
-    userId: {
-      type: Schema.Types.ObjectId,
-      required: true,
-      ref: "user",
-    },
-  },
-  {
-    timestamps: true,
-  }
-);
+//     accountNumber: {
+//       type: Number,
+//       required: true,
+//     },
+//     userId: [{
+//       type: Schema.Types.ObjectId,
+//       required: true,
+//       ref: "user",
+//     }],
+//   },
+//   {
+//     timestamps: true,
+//   }
+// );
 
-AccountSchema.post("validate", function (error, doc, next) {
-  if (error) {
-    error.httpStatusCode = 400;
-    next(error);
-  } else {
-    next();
-  }
-});
+// AccountSchema.post("validate", function (error, doc, next) {
+//   if (error) {
+//     error.httpStatusCode = 400;
+//     next(error);
+//   } else {
+//     next();
+//   }
+// });
 
-AccountSchema.post("save", function (error, doc, next) {
-  if (error.name === "MongoError" && error.code === 11000) {
-    error.httpStatusCode = 400;
-    next(error);
-  } else {
-    next();
-  }
-});
+// AccountSchema.post("save", function (error, doc, next) {
+//   if (error.name === "MongoError" && error.code === 11000) {
+//     error.httpStatusCode = 400;
+//     next(error);
+//   } else {
+//     next();
+//   }
+// });
 
-AccountSchema.post("validate", function (error, doc, next) {
-  if (error) {
-    error.httpStatusCode = 400;
-    next(error);
-  } else {
-    next();
-  }
-});
-AccountSchema.post("save", function (error, doc, next) {
-  if (error.name === "MongoError" && error.code === 11000) {
-    error.httpStatusCode = 400;
-    next(error);
-  } else {
-    next();
-  }
-});
-const AccountModel = model("bank_acount", AccountSchema);
+// AccountSchema.post("validate", function (error, doc, next) {
+//   if (error) {
+//     error.httpStatusCode = 400;
+//     next(error);
+//   } else {
+//     next();
+//   }
+// });
+// AccountSchema.post("save", function (error, doc, next) {
+//   if (error.name === "MongoError" && error.code === 11000) {
+//     error.httpStatusCode = 400;
+//     next(error);
+//   } else {
+//     next();
+//   }
+// });
+// AccountSchema.static("UserAccount", async function(id){
+//   const accounts = await AccountModel.find({_id: id}).populate("users");
+//   return accounts;
+// })  
 
-module.exports = AccountModel;
+// const AccountModels = model("bank_acount", AccountSchema);
+
+// module.exports = AccountModels;
