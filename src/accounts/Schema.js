82adb95a79228dca7f51b110a641d2a91a9965bb
Emmanuel Adedeji <emmans4destiny@gmail.com>
2021-02-10 02:23:05 +0200

    issue on account/users routes

--- src/accounts/Schema.js
+++ src/accounts/Schema.js
@@ -1,70 +1,72 @@
-// const { model, Schema } = require("mongoose");
+const { model, Schema } = require("mongoose");
 
-// const AccountSchema = new Schema(
-//   {
-//     accountName: {
-//       type: String,
-//       required: true,
-//     },
-//     Bank: {
-//       type: String,
-//       required: true,
-//     },
+const AccountSchema = new Schema(
+  {
+    accountName: {
+      type: String,
+      required: true,
+    },
+    Bank: {
+      type: String,
+      required: true,
+    },
 
-//     accountNumber: {
-//       type: Number,
-//       required: true,
-//     },
-//     userId: [{
-//       type: Schema.Types.ObjectId,
-//       required: true,
-//       ref: "user",
-//     }],
-//   },
-//   {
-//     timestamps: true,
-//   }
-// );
+    accountNumber: {
+      type: Number,
+      required: true,
+    },
+    userId: [{
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: "user",
+    }],
+  },
+  {
+    timestamps: true,
+  }
+);
 
-// AccountSchema.post("validate", function (error, doc, next) {
-//   if (error) {
-//     error.httpStatusCode = 400;
-//     next(error);
-//   } else {
-//     next();
-//   }
-// });
+AccountSchema.post("validate", function (error, doc, next) {
+  if (error) {
+    error.httpStatusCode = 400;
+    next(error);
+  } else {
+    next();
+  }
+});
 
-// AccountSchema.post("save", function (error, doc, next) {
-//   if (error.name === "MongoError" && error.code === 11000) {
-//     error.httpStatusCode = 400;
-//     next(error);
-//   } else {
-//     next();
-//   }
-// });
+AccountSchema.post("save", function (error, doc, next) {
+  if (error.name === "MongoError" && error.code === 11000) {
+    error.httpStatusCode = 400;
+    next(error);
+  } else {
+    next();
+  }
+});
 
-// AccountSchema.post("validate", function (error, doc, next) {
-//   if (error) {
-//     error.httpStatusCode = 400;
-//     next(error);
-//   } else {
-//     next();
-//   }
-// });
-// AccountSchema.post("save", function (error, doc, next) {
-//   if (error.name === "MongoError" && error.code === 11000) {
-//     error.httpStatusCode = 400;
-//     next(error);
-//   } else {
-//     next();
-//   }
-// });
-// AccountSchema.static("UserAccount", async function(id){
-//   const accounts = await AccountModel.find({_id: id}).populate("users");
-//   return accounts;
-// })  
+AccountSchema.post("validate", function (error, doc, next) {
+  if (error) {
+    error.httpStatusCode = 400;
+    next(error);
+  } else {
+    next();
+  }
+});
+AccountSchema.post("save", function (error, doc, next) {
+  if (error.name === "MongoError" && error.code === 11000) {
+    error.httpStatusCode = 400;
+    next(error);
+  } else {
+    next();
+  }
+});
 
-// const AccountModels = model("bank_acount", AccountSchema);
+AccountSchema.static("UserAccount", async function(id){
+  const accounts = await AccountModel.find({_id: id}).populate("users");
+  return accounts;
+})  
 
-// module.exports = AccountModels;
+const AccountModel = model("bank_acount", AccountSchema);
+
+
+module.exports = AccountModel;
--- src/accounts/account.js
+++ src/accounts/account.js
@@ -1,82 +1,82 @@
-// const express = require("express");
-// const accountRouter = express.Router();
-// const UserModel = require("../users/Schema");
-// const AccountModel = require("./Schema");
-// const { model, Schema } = require("mongoose");
-// const { authorize, adminOnlyMiddleware } = require("../middlewares/authorize");
+const express = require("express");
+const accountRouter = express.Router();
+const UserModel = require("../users/Schema");
+const AccountModel = require("./Schema");
 
-// accountRouter.get("/", authorize, async (req, res, next) => {
-//   try {
-//     const accountDetails = await AccountModel.find(req.query);
-//     if (accountDetails) {
-//       res
-//         .status(200)
-//         .send({ account: accountDetails, Total: accountDetails.length });
-//     } else {
-//       res.status(400).json({ message: "unable to get account details" });
-//     }
-//   } catch (error) {
-//     next(error);
-//   }
-// });
-// accountRouter.get("/:id", authorize, async (req, res, next) => {
-//   try {
-//     const id = req.params.id;
-//     const accountDetailsId = await AccountModel.UserAccount(id);
+const { authorize, adminOnlyMiddleware } = require("../middlewares/authorize");
 
-//     if (accountDetailsId) {
-//       res.status(200).send(accountDetailsId);
-//     }
-//     res.status(404).json({ message: `Account with ${id} is not Found` });
-//   } catch (error) {
-//     next(error);
-//   }
-// });
-// accountRouter.post("/:id", authorize, async (req, res, next) => {
-//   try {
-//     const id = req.params.id;
-//     const verifyUser = await UserModel.findById(id);
+accountRouter.get("/", authorize, async (req, res, next) => {
+  try {
+    const accountDetails = await AccountModel.find(req.query);
+    if (accountDetails) {
+      res
+        .status(200)
+        .send({ account: accountDetails, Total: accountDetails.length });
+    } else {
+      res.status(400).json({ message: "unable to get account details" });
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+accountRouter.get("/:id", authorize, async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const accountDetailsId = await AccountModel.UserAccount(id);
 
-//     if (verifyUser._id.equals(id)) {
-//       const newAccount = new AccountModel({
-//         ...req.body,
-//         userId: id,
-//       });
-//       const savedAccount = await newAccount.save();
-//       if (savedAccount) {
-//         res.status(201).send(newAccount._id);
-//       } else {
-//         res.status(400).json({ message: "Please check the body and re-post" });
-//       }
-//     } else {
-//       res.status(404).json({ message: `Id with ${id} not valid` });
-//     }
-//   } catch (err) {
-//     console.error(err);
-//     next(err);
-//   }
-// });
-// accountRouter.put("/:id", authorize, async (req, res, next) => {
-//   try {
-//     const id = req.params.id;
-//     const updateAccount = await AccountModel.findByIdAndUpdate(id, req.body);
-//     if (updateAccount) {
-//       res.status(200).send("acount updated successfully");
-//     } else {
-//       res.status(200).json({ message: `User with ${id} not Found` });
-//     }
-//   } catch (error) {
-//     next(error);
-//   }
-// });
-// accountRouter.delete("/:id", authorize, async (req, res, next) => {
-//   const id = req.params.id;
-//   const deleteAccount = await AccountModel.findByIdAndDelete(id);
-//   if (deleteAccount) {
-//     res.status(200).send("Record deleted successfully");
-//   } else {
-//     res.status(404).send(`Record with id ${id} is not found`);
-//   }
-// });
+    if (accountDetailsId) {
+      res.status(200).send(accountDetailsId);
+    }
+    res.status(404).json({ message: `Account with ${id} is not Found` });
+  } catch (error) {
+    next(error);
+  }
+});
+accountRouter.post("/:id", authorize, async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const verifyUser = await UserModel.findById(id);
 
-// module.exports = accountRouter;
+    if (verifyUser._id.equals(id)) {
+      const newAccount = new AccountModel({
+        ...req.body,
+        userId: id,
+      });
+      const savedAccount = await newAccount.save();
+      if (savedAccount) {
+        res.status(201).send(newAccount._id);
+      } else {
+        res.status(400).json({ message: "Please check the body and re-post" });
+      }
+    } else {
+      res.status(404).json({ message: `Id with ${id} not valid` });
+    }
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
+});
+accountRouter.put("/:id", authorize, async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const updateAccount = await AccountModel.findByIdAndUpdate(id, req.body);
+    if (updateAccount) {
+      res.status(200).send("acount updated successfully");
+    } else {
+      res.status(200).json({ message: `User with ${id} not Found` });
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+accountRouter.delete("/:id", authorize, async (req, res, next) => {
+  const id = req.params.id;
+  const deleteAccount = await AccountModel.findByIdAndDelete(id);
+  if (deleteAccount) {
+    res.status(200).send("Record deleted successfully");
+  } else {
+    res.status(404).send(`Record with id ${id} is not found`);
+  }
+});
+
+module.exports = accountRouter;
